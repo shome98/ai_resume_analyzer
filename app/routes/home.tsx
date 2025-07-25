53e7608c0df55460a72acf51c8cb27b370b2899b
Sowparna Shome <soupornoshome@gmail.com>
2025-07-25 23:50:42 +0530

    navbar use

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,3 +1,4 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
 
 export function meta({}: Route.MetaArgs) {
@@ -11,6 +12,7 @@ export default function Home() {
   return (
     // need to change the bg as it is rounded on top side make it bit eye friendly
     <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar/>
       <section className="main-section">
         <div className="page-heading">
           <h1>Track your Appliaction & Resume Ratings</h1>
