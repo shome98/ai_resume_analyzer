f9b74f01c6123d38493b8294324aca9b68baf85c
Sowparna Shome <soupornoshome@gmail.com>
2025-07-25 23:24:01 +0530

    add heading check

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,22 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "Analyse Your Resume" },
-    { name: "description", content: "Welcome to React Router!" },
+    { name: "description", content: "Get feedback on your resume!." },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    // need to change the bg as it is rounded on top side make it bit eye friendly
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <section className="main-section">
+        <div className="page-heading">
+          <h1>Track your Appliaction & Resume Ratings</h1>
+          <h2>Review your submissions and receive AI-powered feedback.</h2>
+        </div>
+      </section>
+    </main>
+  );
 }
