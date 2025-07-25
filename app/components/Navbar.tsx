ae3a312c054788a77261e98fc0bff5bbf5a24421
Sowparna Shome <soupornoshome@gmail.com>
2025-07-25 23:50:36 +0530

    navabr create

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router";
+
+const Navbar: React.FC = () => {
+    return <nav className="navbar">
+        <Link to="/">
+            <p className="text-2xl font-bold text-gradient">ResuAi</p>
+        </Link>
+        <Link to="/unpload" className="primary-button w-fit">Upload Resume</Link>
+  </nav>;
+};
+
+export default Navbar;
